1b352c8eca063b0e344c4305d91f1b02d75def6f
apetor56 <lukten.github@gmail.com>
2022-11-28 18:25:03 +0100

    accepted

--- zestaw-04/zad_4_02.py
+++ zestaw-04/zad_4_02.py
@@ -1,7 +1,7 @@
 # Rozwiązania zadań 3.5 i 3.6 z poprzedniego zestawu zapisać w postaci funkcji, które zwracają pełny string przez return.
 # Funkcje nie powinny pytać użytkownika o dane, tylko korzystać z argumentów.
 
-def make_ruler(n):
+def make_ruler(n): 
     miarka = '|'
     num = '0'
 
