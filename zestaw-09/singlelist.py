57230299b183d6085f6e965a5e2cec88b210b5f0
apetor56 <lukten.github@gmail.com>
2022-12-20 11:30:07 +0100

    done

---
+++ zestaw-09/singlelist.py
@@ -0,0 +1,82 @@
+class Node:
+    """Klasa reprezentująca węzeł listy jednokierunkowej."""
+
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return str(self.data)   # bardzo ogólnie
+
+class SingleList:
+    """Klasa reprezentująca całą listę jednokierunkową."""
+
+    def __init__(self):
+        self.length = 0   # nie trzeba obliczać za każdym razem
+        self.head = None
+        self.tail = None
+
+    def is_empty(self):
+        #return self.length == 0
+        return self.head is None
+
+    def count(self):   # tworzymy interfejs do odczytu
+        return self.length
+
+    def insert_head(self, node):
+        if self.head:   # dajemy na koniec listy
+            node.next = self.head
+            self.head = node
+        else:   # pusta lista
+            self.head = self.tail = node
+        self.length += 1
+
+    def insert_tail(self, node):   # klasy O(1)
+        if self.head:   # dajemy na koniec listy
+            self.tail.next = node
+            self.tail = node
+        else:   # pusta lista
+            self.head = self.tail = node
+        self.length += 1
+
+    def remove_head(self):   # klasy O(1)
+        if self.is_empty():
+            raise ValueError("pusta lista")
+        node = self.head
+        if self.head == self.tail:   # self.length == 1
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+        node.next = None   # czyszczenie łącza
+        self.length -= 1
+        return node   # zwracamy usuwany node
+
+    def remove_tail(self):
+        if self.is_empty():
+            raise ValueError("pusta lista")
+        node = self.tail
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            current = self.head
+            while current.next != self.tail:
+                current = current.next
+            current.next = None
+            self.tail = current
+        self.length -= 1
+        return node
+
+    def join(self, other):
+        if self.is_empty():
+            self.head = other.head
+            self.tail = other.tail
+            self.length = other.length
+        elif not other.is_empty():
+            self.tail.next = other.head
+            self.tail = other.tail
+            self.length += other.length
+        other.clear()
+
+    def clear(self):
+        while not self.is_empty():
+            self.remove_head()
