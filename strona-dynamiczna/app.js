6e5bc766401a91c3b7da65ce7fde3874c73d7bf4
apetor56 <lukten.github@gmail.com>
2023-02-19 16:39:13 +0100

    removed unnecessary modules

--- strona-dynamiczna/app.js
+++ strona-dynamiczna/app.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
-const ejs = require('ejs');
 
 const register_route = require('./routes/register');
 const login_route = require('./routes/login');
